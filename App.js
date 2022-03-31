928aee7cd6926a4b949f1899a9a1c69fa29acab1
Ryan Matheus <ryanmatheusdev@gmail.com>
2022-03-31 17:41:47 -0300

    .

---
+++ App.js
@@ -0,0 +1,167 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 8877
+
+app.get('/', (req, res) => {
+    res.json({msg: 'ok'})
+})
+
+app.get('/produtos', (req, res) => {
+    res.json(
+        {
+            "produtos":[
+                {
+                    "title": "Promoções",
+                    "X-Bacon": {
+                    "id": "1",
+                    "title": "X-Bacon",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    },
+                    "Quarteirão": {
+                    "id": "1",
+                    "title": "Quarteirão",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    },
+                    "X-Picanha": {
+                    "id": "1",
+                    "title": "X-Picanha",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    }
+                },
+                {
+                    "title": "Burguer",
+                    "Chicken Burguer": {
+                    "id": "1",
+                    "title": "Chicken Burguer",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    },
+                    "Chicken Burguer Jr": {
+                    "id": "1",
+                    "title": "Chicken Burguer Jr",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    },
+                    "Cheese Burguer": {
+                    "id": "1",
+                    "title": "Cheese Burguer",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    }
+                },
+                {
+                    "title": "Sobremesa",
+                    "Sorvete": {
+                    "id": "1",
+                    "title": "Sorvete",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    },
+                    "Pudim": {
+                    "id": "1",
+                    "title": "Pudim",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    },
+                    "Petigatot": {
+                    "id": "1",
+                    "title": "Petigatot",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    }
+                },
+                {
+                    "title": "Vegetarianos",
+                    "Sorvete": {
+                    "id": "1",
+                    "title": "Sorvete",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    },
+                    "Pudim": {
+                    "id": "1",
+                    "title": "Pudim",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    },
+                    "Petigatot": {
+                    "id": "1",
+                    "title": "Petigatot",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    }
+                },
+                {
+                    "title": "Porções",
+                    "Sorvete": {
+                    "id": "1",
+                    "title": "Sorvete",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    },
+                    "Pudim": {
+                    "id": "1",
+                    "title": "Pudim",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    },
+                    "Petigatot": {
+                    "id": "1",
+                    "title": "Petigatot",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    }
+                },
+                {
+                    "title": "Bebidas",
+                    "Sorvete": {
+                    "id": "1",
+                    "title": "Sorvete",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    },
+                    "Pudim": {
+                    "id": "1",
+                    "title": "Pudim",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    },
+                    "Petigatot": {
+                    "id": "1",
+                    "title": "Petigatot",
+                    "description":
+                        "Pão, Blend 100g, 1 fatia de queijo cheedar, cebola, picles com ketchup e mostarda.",
+                    "price": 10.5
+                    }
+                }
+            ]
+        }
+        
+            
+    )
+})
+
+app.listen(PORT, () => {
+    console.log('Escutando na porta ' + PORT)
+})
